65850c408bbf2b71194b7b03ee41b469a010d229
tusharjagi <tusharjagi@gmail.com>
2022-11-14 15:47:27 +0530

    Content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import Content from './Components/Content/Content';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <>
       <Header/>
-
+      <Content/>
     </>
   );
 }

b6ffcca64f1680f270638d55d8d783e03f0f7f72
tusharjagi <tusharjagi@gmail.com>
2022-11-14 15:39:54 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo app</h1>
-    </div>
+    <>
+      <Header/>
+
+    </>
   );
 }
 

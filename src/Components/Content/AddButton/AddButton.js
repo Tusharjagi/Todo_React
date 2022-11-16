4023776b841fcdde90289f486385e5de8f3c41ac
tusharjagi <tusharjagi@gmail.com>
2022-11-16 12:16:32 +0530

    add button

--- src/Components/Content/AddButton/AddButton.js
+++ src/Components/Content/AddButton/AddButton.js
@@ -1,7 +1,7 @@
 import "./add-button.css";
 
 const AddButton = () => {
-  return <button className="add-button">Add</button>;
+  return <button className="add-button">ADD</button>;
 };
 
 export default AddButton;

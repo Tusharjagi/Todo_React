eaf2b05ca96995745090f96a0dc1bced1a45d327
tusharjagi <tusharjagi@gmail.com>
2022-11-16 17:35:21 +0530

    style

--- src/Components/Content/Input/Calender/Calender.js
+++ src/Components/Content/Input/Calender/Calender.js
@@ -1,15 +1,10 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { Icon } from '@mui/material';
-import './calender.css';
+import * as React from "react";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import "./calender.css";
 const Calender = () => {
   const [value, setValue] = React.useState(dayjs());
 
@@ -18,18 +13,17 @@ const Calender = () => {
   };
 
   return (
-    <div className='calender' >
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <div className="calender">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateTimePicker
           label="Set Time & Date"
           value={value}
           onChange={handleChange}
           renderInput={(props) => <TextField {...props} />}
         />
-
-    </LocalizationProvider>
+      </LocalizationProvider>
     </div>
   );
-}
+};
 
 export default Calender;
